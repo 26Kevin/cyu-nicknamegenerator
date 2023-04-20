6501817446d2232144a141ded24013daa43e1978
26Kevin <113374266+26Kevin@users.noreply.github.com>
2023-04-20 11:23:19 -0600

    updating

---
+++ main.js
@@ -0,0 +1,14 @@
+// Nickname Generator CYU Assignment Kevin Patel
+
+let nicknames = [
+  "Crusher",
+  "the Scientist",
+  "Twinkle-toes",
+  "the Coder",
+  "the Jester",
+  "the Sloth",
+  "Quick-Silver",
+];
+
+let firstData;
+fetch("data/");
