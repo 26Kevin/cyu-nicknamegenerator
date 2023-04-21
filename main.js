ae54b3c90d394f7778ae51eced5a2cfc5c074130
26Kevin <113374266+26Kevin@users.noreply.github.com>
2023-04-21 11:23:11 -0600

    updating

--- main.js
+++ main.js
@@ -1,14 +1,37 @@
 // Nickname Generator CYU Assignment Kevin Patel
 
-let nicknames = [
-  "Crusher",
-  "the Scientist",
-  "Twinkle-toes",
-  "the Coder",
-  "the Jester",
-  "the Sloth",
-  "Quick-Silver",
-];
+// Load data from files
+let nicknameData;
+fetch("data/nicknamedata.txt")
+  .then((rawData) => rawData.text())
+  .then((strData) => (nicknameData = strData.split(/\r?\n/)));
 
 // Output
-let outputEl = document.getElementById
+let outputEl = document.getElementById("output");
+
+// Button Event Listener
+document.getElementById("btn").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  let selection = document.getElementById("nickname-select").value;
+
+  // Process Menu Selection
+  if (selection === "displayall") {
+    traverseDisplayAll();
+  } else if (selection === "displayrandom") {
+    traverseDisplayRandom();
+  } else if (selection === "add") {
+    traverseAdd();
+  } else if (selection === "removelast") {
+    traverseRemoveLast();
+  } else if (selection === "removeindex") {
+    traverseRemoveIndex();
+  } else if (selection === "removename") {
+    traverseRemoveName();
+  }
+}
+
+// Menu Option Functions
+function traverseDisplayAll() {
+  outputEl.innerHTML = surveyData;
+}
